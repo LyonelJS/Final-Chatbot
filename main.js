2ec6ba7276c4e38b3bec95075b80bcc21954b7ae
LyonelJS <lyonel.saputra@binus.ac.id>
2024-12-01 21:01:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -255,7 +255,7 @@ const generateAPIResponse = async () => {
       const botMessageObj = { sender: "bot", message: apiResponse };
       chatHistory[currentChatIndex].messages.push(botMessageObj);
       localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
-promptInput.value = '';
+
       // Scroll to the latest message
       chatContainer.scrollTop = chatContainer.scrollHeight;
 
@@ -269,7 +269,7 @@ promptInput.value = '';
   } catch (error) {
       console.error(error);
   } 
-    
+    promptInput.value = '';
 
 };
 
